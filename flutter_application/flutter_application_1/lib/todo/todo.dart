6652983bc83b561bb550618053d38092eca3baca
tarik <arifikbaltarik@gmail.com>
2024-05-20 23:52:25 +0600

    this is to do app

--- flutter_application/flutter_application_1/lib/todo/todo.dart
+++ flutter_application/flutter_application_1/lib/todo/todo.dart
@@ -1,109 +1,82 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(HomePage());
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Tarik Todo App"),
+        backgroundColor: Colors.red,
+      ),
+      body: TarikTodoApp(),
+    ),
+  ));
 }
 
-class HomePage extends StatelessWidget {
+class TarikTodoApp extends StatefulWidget {
+  const TarikTodoApp({super.key});
+
+  @override
+  State<TarikTodoApp> createState() => _TarikTodoAppState();
+}
+
+class _TarikTodoAppState extends State<TarikTodoApp> {
+  final List<String> _list = [];
+  final TextEditingController _controller = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "TO-DO",
-        home: Scaffold(
-            backgroundColor: Color.fromARGB(255, 236, 236, 235),
-            appBar: AppBar(
-              elevation: 0,
-              centerTitle: true,
-              backgroundColor: Colors.white,
-              actions: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Icon(
-                    Icons.add,
-                    color: Colors.black,
-                  ),
-                )
-              ],
-              title: Text(
-                "TO-DO",
-                style: TextStyle(
-                  color: Colors.black,
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: "Enter your todo here",
                 ),
-              ),
-              leading: Icon(
-                Icons.menu,
-                color: Colors.black,
               ),
             ),
-            body: ListView(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 20, top: 40),
-                  child: Text(
-                    "All TO-DO Tasks",
-                    style: TextStyle(
-                      color: const Color.fromARGB(224, 0, 0, 0),
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+            IconButton(
+              onPressed: () {
+                if (_controller.text.isNotEmpty) {
+                  setState(() {
+                    _list.add(_controller.text);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Todo added")),
+                    );
+                    _controller.clear();
+                  });
+                }
+              },
+              icon: Icon(Icons.add),
+            ),
+          ],
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _list.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(_list[index]),
+                trailing: IconButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("${_list[index]} deleted")),
+                    );
+                    setState(() {
+                      _list.removeAt(index);
+                    });
+                  },
+                  icon: Icon(Icons.delete),
                 ),
-                SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                  child: Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 233, 183, 47),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Padding(
-                        padding: const EdgeInsets.all(
-                          8.0,
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.all(4.0),
-                              child: Text(
-                                "You have to remember that allah is always looking at you",
-                                style: TextStyle(fontSize: 16),
-                              ),
-                            ),
-                            SizedBox(height: 10),
-                            Divider(
-                              color: Color.fromARGB(255, 39, 21, 21),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.edit,
-                                    color: Colors.greenAccent,
-                                  ),
-                                  Icon(
-                                    Icons.delete,
-                                    color:
-                                        const Color.fromARGB(255, 228, 244, 54),
-                                  ),
-                                  Icon(
-                                    Icons.done,
-                                    color: Colors.blueGrey,
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        )),
-                  ),
-                ),
-              ],
-            )));
+              );
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
